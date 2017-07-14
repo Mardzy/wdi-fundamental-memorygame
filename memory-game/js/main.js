8dacf0467ff7ffcbd8b614e63a3e89ff103b6fd3
Paul Mardling <mardzy@Yours-MacBook-Pro.local>
2017-07-14 09:01:10 +0100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,23 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [{
+        suit: 'Hearts',
+        rank: 'Queen',
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        suit: 'Diamonds',
+        rank: 'Queen',
+        cardImage: "images/queen-of-diamonds.png",
+    }, {
+        suit: 'Hearts',
+        rank: 'King',
+        cardImage: "images/king-of-hearts.png",
+    }, {
+        suit: 'Diamonds',
+        rank: 'King',
+        cardImage: "images/king-of-diamonds.png",
+    }
+];
+
 var checkForMatch = function() {
     if (cardsInPlay[0] === cardsInPlay[2]) {
         alert('You have found a match');
@@ -6,12 +25,17 @@ var checkForMatch = function() {
         alert('Sorry, try again.');
     }
 }
+
 var cardsInPlay = [];
 
 var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
+
+
 }
 flipCard(0);
 flipCard(2);
